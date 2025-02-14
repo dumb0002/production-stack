b407b78b4d1067d7590bb26566c71224e1224cba
Braulio Dumba <brauliodumba@gmail.com>
2025-02-13 21:48:36 -0500

    latest
    Signed-off-by: Braulio Dumba <brauliodumba@gmail.com>

--- perf-test/server-startup/vllmLogParser.py
+++ perf-test/server-startup/vllmLogParser.py
@@ -3,8 +3,158 @@ import re
 from datetime import datetime
 
 
+
+# Extract Engine Initialization
+def get_engine_init_time(logs):
+    pattern1 = "Automatically detected platform cuda."
+    pattern2 = "Starting to load model"
+
+    t = 0
+    m1=False
+    m2=False
+    
+    for line in logs:
+        sentence = line.strip("\n")
+        match_1 = re.search(pattern1, sentence)
+        match_2 = re.search(pattern2, sentence)
+
+        if match_1 and not m1:
+           t1 = sentence.split(" ")[2]
+           m1=True
+        elif match_2 and not m2:
+           t2 = sentence.split(" ")[2]
+           m2=True
+        else:
+           continue
+
+        # compute the time
+        if m1 and m2:
+           format_string = "%H:%M:%S"
+           d1 = datetime.strptime(t1, format_string)
+           d2 = datetime.strptime(t2, format_string)
+           delta = d2 - d1
+           t = delta.seconds
+           break
+    return t
+
+
+# Extract Model Loading latency
+def get_model_load_time(logs):
+    pattern1 = "Starting to load model"
+    pattern2 = "Loading model weights took"
+    #pattern2 = "Capturing cudagraphs for decoding"
+    
+    t = 0
+    m1=False
+    m2=False
+    
+    for line in logs:
+        sentence = line.strip("\n")
+        match_1 = re.search(pattern1, sentence)
+        match_2 = re.search(pattern2, sentence)
+
+        if match_1 and not m1:
+           t1 = sentence.split(" ")[2]
+           m1=True
+        elif match_2 and not m2:
+           t2 = sentence.split(" ")[2]
+           m2=True
+        else:
+           continue
+
+        # compute the time
+        if m1 and m2:
+           format_string = "%H:%M:%S"
+           d1 = datetime.strptime(t1, format_string)
+           d2 = datetime.strptime(t2, format_string)
+           delta = d2 - d1
+           t = delta.seconds
+           break
+    return t
+
+
+# Extract Model weight loading (GB)
+def get_model_weight_gb(logs):
+    t = 0
+    pattern = "Loading model weights took"
+    
+    for line in logs:
+        sentence = line.strip("\n")
+        match = re.search(pattern, sentence)
+
+        if match:
+            new_sentence=sentence.split("]")
+            match = re.findall(r"-?\d+\.\d+|-?\d+", new_sentence[1])  # The regex pattern matches one or more digits (\d+)
+            
+            if match:
+               t = match[0]
+               break
+            else:
+                continue
+        else:
+            continue
+    return t
+
+
+# Extract Torch compile time
+def get_torch_compile_time(logs):
+    t = 0
+    pattern = "torch.compile takes"
+    
+    for line in logs:
+        sentence = line.strip("\n")
+        match = re.search(pattern, sentence)
+
+        if match:
+            new_sentence=sentence.split("]")
+            match = re.findall(r"-?\d+\.\d+|-?\d+", new_sentence[1])  # The regex pattern matches one or more digits (\d+)
+
+            if match:
+               t = match[0]
+               break
+            else:
+                continue
+        else:
+            continue
+    return t
+
+
+# Extract before Torch compile time
+def get_before_torch_compile_time(logs):
+    pattern1 = "Loading model weights took"
+    pattern2 = "torch.compile takes"
+    
+    t = 0
+    m1=False
+    m2=False
+    
+    for line in logs:
+        sentence = line.strip("\n")
+        match_1 = re.search(pattern1, sentence)
+        match_2 = re.search(pattern2, sentence)
+
+        if match_1 and not m1:
+           t1 = sentence.split(" ")[2]
+           m1=True
+        elif match_2 and not m2:
+           t2 = sentence.split(" ")[2]
+           m2=True
+        else:
+           continue
+
+        # compute the time
+        if m1 and m2:
+           format_string = "%H:%M:%S"
+           d1 = datetime.strptime(t1, format_string)
+           d2 = datetime.strptime(t2, format_string)
+           delta = d2 - d1
+           t = delta.seconds
+           break
+    return t
+
+
 # Extract CUDA graph instantiation latency
-def get_graph_capture_time(logs):
+def get_cuda_graph_time(logs):
     t = 0
     pattern = "Graph capturing finished"
 
@@ -25,6 +175,56 @@ def get_graph_capture_time(logs):
             continue
     return t
 
+
+# # Extract init engine time
+# def get_apiserver_init_time (logs):
+#     d = None
+#     pattern = "init engine"
+
+#     for line in logs:
+#         sentence = line.strip("\n")
+#         match = re.search(pattern, sentence)
+       
+#         if match:
+#            format_string = "%H:%M:%S"
+#            t = sentence.split(" ")[2]
+#            d = datetime.strptime(t, format_string)
+#         else:
+#            continue 
+#     return d
+
+# Extract API Readiness time
+def get_apiserver_init_time (logs):
+    pattern1 = "init engine"
+    pattern2 = "Route: /invocations, Methods: POST"
+    
+    t = 0
+    m1=False
+    m2=False
+    
+    for line in logs:
+        sentence = line.strip("\n")
+        match_1 = re.search(pattern1, sentence)
+        match_2 = re.search(pattern2, sentence)
+
+        if match_1 and not m1:
+           t1 = sentence.split(" ")[2]
+           m1=True
+        elif match_2 and not m2:
+           t2 = sentence.split(" ")[2]
+           m2=True
+        else:
+           continue
+
+        # compute the time
+        if m1 and m2:
+           format_string = "%H:%M:%S"
+           d1 = datetime.strptime(t1, format_string)
+           d2 = datetime.strptime(t2, format_string)
+           delta = d2 - d1
+           t = delta.seconds
+           break
+    return t
 
 # Extract memory profile latency
 def get_memory_profile_time(logs):
@@ -72,102 +272,10 @@ def get_model_weight_time(logs):
     return t
 
 
-# Extract Engine Initialization
-def get_engine_init_time(logs):
-    pattern1 = "Automatically detected platform cuda."
-    pattern2 = "Starting to load model"
+def convert_string_to_datetime(date_string, format_string):
+    try:
+      datetime_object = datetime.strptime(date_string, format_string)
+      return datetime_object
 
-    t = 0
-    m1=False
-    m2=False
-    
-    for line in logs:
-        sentence = line.strip("\n")
-        match_1 = re.search(pattern1, sentence)
-        match_2 = re.search(pattern2, sentence)
-
-        if match_1 and not m1:
-           t1 = sentence.split(" ")[2]
-           m1=True
-        elif match_2 and not m2:
-           t2 = sentence.split(" ")[2]
-           m2=True
-        else:
-           continue
-
-        # compute the time
-        if m1 and m2:
-           format_string = "%H:%M:%S"
-           d1 = datetime.strptime(t1, format_string)
-           d2 = datetime.strptime(t2, format_string)
-           delta = d2 - d1
-           t = delta.seconds
-           break
-    return t
-
-
-# Extract Model Loading latency
-def get_model_load_time(logs):
-    pattern1 = "Starting to load model"
-    pattern2 = "Capturing cudagraphs for decoding"
-    
-    t = 0
-    m1=False
-    m2=False
-    
-    for line in logs:
-        sentence = line.strip("\n")
-        match_1 = re.search(pattern1, sentence)
-        match_2 = re.search(pattern2, sentence)
-
-        if match_1 and not m1:
-           t1 = sentence.split(" ")[2]
-           m1=True
-        elif match_2 and not m2:
-           t2 = sentence.split(" ")[2]
-           m2=True
-        else:
-           continue
-
-        # compute the time
-        if m1 and m2:
-           format_string = "%H:%M:%S"
-           d1 = datetime.strptime(t1, format_string)
-           d2 = datetime.strptime(t2, format_string)
-           delta = d2 - d1
-           t = delta.seconds
-           break
-    return t
-
-
-def get_apiserver_init_time (logs):
-    pattern1 = "init engine"
-    pattern2 = "Route: /invocations, Methods: POST"
-    
-    t = 0
-    m1=False
-    m2=False
-    
-    for line in logs:
-        sentence = line.strip("\n")
-        match_1 = re.search(pattern1, sentence)
-        match_2 = re.search(pattern2, sentence)
-
-        if match_1 and not m1:
-           t1 = sentence.split(" ")[2]
-           m1=True
-        elif match_2 and not m2:
-           t2 = sentence.split(" ")[2]
-           m2=True
-        else:
-           continue
-
-        # compute the time
-        if m1 and m2:
-           format_string = "%H:%M:%S"
-           d1 = datetime.strptime(t1, format_string)
-           d2 = datetime.strptime(t2, format_string)
-           delta = d2 - d1
-           t = delta.seconds
-           break
-    return t
+    except ValueError:
+      return None
