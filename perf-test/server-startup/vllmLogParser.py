ea2c68144a5d7570bfd06760e77a81266284d05d
Braulio Dumba <brauliodumba@gmail.com>
2025-02-16 17:30:40 -0500

    latest
    Signed-off-by: Braulio Dumba <brauliodumba@gmail.com>

--- perf-test/server-startup/vllmLogParser.py
+++ perf-test/server-startup/vllmLogParser.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 import re
+import pytz
 from datetime import datetime
 
 
@@ -176,55 +177,60 @@ def get_cuda_graph_time(logs):
     return t
 
 
-# # Extract init engine time
+# Extract init engine time
+def get_apiserver_init_time (logs):
+    d = None
+    pattern = "init engine"
+
+    for line in logs:
+        sentence = line.strip("\n")
+        match = re.search(pattern, sentence)
+
+        if match:
+           current_datetime = datetime.now()
+           current_year = current_datetime.year
+
+           t = str(current_year) + "-" + sentence.split(" ")[1] + " " + sentence.split(" ")[2]
+
+           # convert to UTC timezone:
+           d = convert_pt_to_utc(t)
+        else:
+           continue 
+    return d
+
+
+# # Extract API Readiness time
 # def get_apiserver_init_time (logs):
-#     d = None
-#     pattern = "init engine"
+#     pattern1 = "init engine"
+#     pattern2 = "Route: /invocations, Methods: POST"
+    
+#     t = 0
+#     m1=False
+#     m2=False
 
 #     for line in logs:
 #         sentence = line.strip("\n")
-#         match = re.search(pattern, sentence)
-       
-#         if match:
-#            format_string = "%H:%M:%S"
-#            t = sentence.split(" ")[2]
-#            d = datetime.strptime(t, format_string)
+#         match_1 = re.search(pattern1, sentence)
+#         match_2 = re.search(pattern2, sentence)
+
+#         if match_1 and not m1:
+#            t1 = sentence.split(" ")[2]
+#            m1=True
+#         elif match_2 and not m2:
+#            t2 = sentence.split(" ")[2]
+#            m2=True
 #         else:
-#            continue 
-#     return d
+#            continue
 
-# Extract API Readiness time
-def get_apiserver_init_time (logs):
-    pattern1 = "init engine"
-    pattern2 = "Route: /invocations, Methods: POST"
-    
-    t = 0
-    m1=False
-    m2=False
-    
-    for line in logs:
-        sentence = line.strip("\n")
-        match_1 = re.search(pattern1, sentence)
-        match_2 = re.search(pattern2, sentence)
-
-        if match_1 and not m1:
-           t1 = sentence.split(" ")[2]
-           m1=True
-        elif match_2 and not m2:
-           t2 = sentence.split(" ")[2]
-           m2=True
-        else:
-           continue
-
-        # compute the time
-        if m1 and m2:
-           format_string = "%H:%M:%S"
-           d1 = datetime.strptime(t1, format_string)
-           d2 = datetime.strptime(t2, format_string)
-           delta = d2 - d1
-           t = delta.seconds
-           break
-    return t
+#         # compute the time
+#         if m1 and m2:
+#            format_string = "%H:%M:%S"
+#            d1 = datetime.strptime(t1, format_string)
+#            d2 = datetime.strptime(t2, format_string)
+#            delta = d2 - d1
+#            t = delta.seconds
+#            break
+#     return t
 
 # Extract memory profile latency
 def get_memory_profile_time(logs):
@@ -279,3 +285,16 @@ def convert_string_to_datetime(date_string, format_string):
 
     except ValueError:
       return None
+
+
+def convert_pt_to_utc(pt_datetime_str, pt_timezone_str='US/Pacific'):
+    pt_timezone = pytz.timezone(pt_timezone_str)
+    pt_datetime = datetime.strptime(pt_datetime_str, "%Y-%m-%d %H:%M:%S")
+    
+    # Make the datetime object timezone-aware
+    pt_datetime_aware = pt_timezone.localize(pt_datetime)
+    
+    # Convert to UTC
+    utc_datetime = pt_datetime_aware.astimezone(pytz.utc)
+    
+    return utc_datetime
