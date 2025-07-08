3c2f420ac3f6004077058081518f8d9a0a209614
Braulio Dumba <brauliodumba@gmail.com>
2025-07-08 18:02:50 -0400

    Update parserHelper.py

--- perf-test/server-startup/parserHelper.py
+++ perf-test/server-startup/parserHelper.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
 import re
-import pytz
 from datetime import datetime
+
+import pytz
 
 
 # Extract Process Startup end time: vllm_first_log_message_timestamp
-def get_log_first_timestamp (logs):
+def get_log_first_timestamp(logs):
     d = None
     pattern = "Automatically detected platform cuda."
 
@@ -14,15 +15,21 @@ def get_log_first_timestamp (logs):
         match = re.search(pattern, sentence)
 
         if match:
-           current_datetime = datetime.now()
-           current_year = current_datetime.year
-           t = str(current_year) + "-" + sentence.split(" ")[1] + " " + sentence.split(" ")[2]
+            current_datetime = datetime.now()
+            current_year = current_datetime.year
+            t = (
+                str(current_year)
+                + "-"
+                + sentence.split(" ")[1]
+                + " "
+                + sentence.split(" ")[2]
+            )
 
-           # convert to UTC timezone:
-           d = convert_pt_to_utc(t)
-           break
+            # convert to UTC timezone:
+            d = convert_pt_to_utc(t)
+            break
         else:
-           continue 
+            continue
     return d
 
 
@@ -32,31 +39,31 @@ def get_engine_init_time(logs):
     pattern2 = "Starting to load model"
 
     t = 0
-    m1=False
-    m2=False
-    
+    m1 = False
+    m2 = False
+
     for line in logs:
         sentence = line.strip("\n")
         match_1 = re.search(pattern1, sentence)
         match_2 = re.search(pattern2, sentence)
 
         if match_1 and not m1:
-           t1 = sentence.split(" ")[2]
-           m1=True
+            t1 = sentence.split(" ")[2]
+            m1 = True
         elif match_2 and not m2:
-           t2 = sentence.split(" ")[2]
-           m2=True
+            t2 = sentence.split(" ")[2]
+            m2 = True
         else:
-           continue
+            continue
 
         # compute the time
         if m1 and m2:
-           format_string = "%H:%M:%S"
-           d1 = datetime.strptime(t1, format_string)
-           d2 = datetime.strptime(t2, format_string)
-           delta = d2 - d1
-           t = delta.seconds
-           break
+            format_string = "%H:%M:%S"
+            d1 = datetime.strptime(t1, format_string)
+            d2 = datetime.strptime(t2, format_string)
+            delta = d2 - d1
+            t = delta.seconds
+            break
     return t
 
 
@@ -64,18 +71,19 @@ def get_engine_init_time(logs):
 def get_model_weight_download_time(logs):
     t = 0
     pattern = "Time spent downloading weights"
-    
+
     for line in logs:
         sentence = line.strip("\n")
         match = re.search(pattern, sentence)
-
         if match:
-            new_sentence=(sentence.split("]")[1]).split(":")
-            match = re.findall(r"-?\d+\.\d+|-?\d+", new_sentence[1])  # The regex pattern matches one or more digits (\d+)
+            new_sentence = (sentence.split("]")[1]).split(":")
+            match = re.findall(
+                r"-?\d+\.\d+|-?\d+", new_sentence[1]
+            )  # The regex pattern matches one or more digits (\d+)
 
             if match:
-               t = match[0]
-               break
+                t = match[0]
+                break
             else:
                 continue
         else:
@@ -83,91 +91,48 @@ def get_model_weight_download_time(logs):
     return t
 
 
-# Extract Model Weight Load latency 
+# Extract Model Weight Load latency
 def get_model_weight_load_time(logs):
-    pattern1 = "Time spent downloading weights"
-    pattern2 = "Loading model weights took"
-    #pattern2 = "Capturing cudagraphs for decoding"
-    
     t = 0
-    m1=False
-    m2=False
-    
+    pattern = "Loading weights took"
+
     for line in logs:
         sentence = line.strip("\n")
-        match_1 = re.search(pattern1, sentence)
-        match_2 = re.search(pattern2, sentence)
+        match = re.search(pattern, sentence)
+        if match:
+            new_sentence = sentence.split("]")[1]
+            match = re.findall(
+                r"-?\d+\.\d+|-?\d+", new_sentence
+            )  # The regex pattern matches one or more digits (\d+)
 
-        if match_1 and not m1:
-           t1 = sentence.split(" ")[2]
-           m1=True
-        elif match_2 and not m2:
-           t2 = sentence.split(" ")[2]
-           m2=True
+            if match:
+                t = match[0]
+                break
+            else:
+                continue
         else:
-           continue
-
-        # compute the time
-        if m1 and m2:
-           format_string = "%H:%M:%S"
-           d1 = datetime.strptime(t1, format_string)
-           d2 = datetime.strptime(t2, format_string)
-           delta = d2 - d1
-           t = delta.seconds
-           break
-    return t
-
-# Extract Model Loading latency
-def get_model_load_time(logs):
-    pattern1 = "Starting to load model"
-    pattern2 = "Loading model weights took"
-    #pattern2 = "Capturing cudagraphs for decoding"
-    
-    t = 0
-    m1=False
-    m2=False
-    
-    for line in logs:
-        sentence = line.strip("\n")
-        match_1 = re.search(pattern1, sentence)
-        match_2 = re.search(pattern2, sentence)
-
-        if match_1 and not m1:
-           t1 = sentence.split(" ")[2]
-           m1=True
-        elif match_2 and not m2:
-           t2 = sentence.split(" ")[2]
-           m2=True
-        else:
-           continue
-
-        # compute the time
-        if m1 and m2:
-           format_string = "%H:%M:%S"
-           d1 = datetime.strptime(t1, format_string)
-           d2 = datetime.strptime(t2, format_string)
-           delta = d2 - d1
-           t = delta.seconds
-           break
+            continue
     return t
 
 
 # Extract Model weight loading (GB)
 def get_model_weight_gb(logs):
     t = 0
-    pattern = "Loading model weights took"
-    
+    pattern = "Model loading took"
+
     for line in logs:
         sentence = line.strip("\n")
         match = re.search(pattern, sentence)
 
         if match:
-            new_sentence=sentence.split("]")
-            match = re.findall(r"-?\d+\.\d+|-?\d+", new_sentence[1])  # The regex pattern matches one or more digits (\d+)
-            
+            new_sentence = sentence.split("]")
+            match = re.findall(
+                r"-?\d+\.\d+|-?\d+", new_sentence[1]
+            )  # The regex pattern matches one or more digits (\d+)
+
             if match:
-               t = match[0]
-               break
+                t = match[0]
+                break
             else:
                 continue
         else:
@@ -179,18 +144,20 @@ def get_model_weight_gb(logs):
 def get_torch_compile_time(logs):
     t = 0
     pattern = "torch.compile takes"
-    
+
     for line in logs:
         sentence = line.strip("\n")
         match = re.search(pattern, sentence)
 
         if match:
-            new_sentence=sentence.split("]")
-            match = re.findall(r"-?\d+\.\d+|-?\d+", new_sentence[1])  # The regex pattern matches one or more digits (\d+)
+            new_sentence = sentence.split("]")
+            match = re.findall(
+                r"-?\d+\.\d+|-?\d+", new_sentence[1]
+            )  # The regex pattern matches one or more digits (\d+)
 
             if match:
-               t = match[0]
-               break
+                t = match[0]
+                break
             else:
                 continue
         else:
@@ -200,35 +167,35 @@ def get_torch_compile_time(logs):
 
 # Extract before Torch compile time
 def get_before_torch_compile_time(logs):
-    pattern1 = "Loading model weights took"
+    pattern1 = "Loading weights took"
     pattern2 = "torch.compile takes"
-    
+
     t = 0
-    m1=False
-    m2=False
-    
+    m1 = False
+    m2 = False
+
     for line in logs:
         sentence = line.strip("\n")
         match_1 = re.search(pattern1, sentence)
         match_2 = re.search(pattern2, sentence)
 
         if match_1 and not m1:
-           t1 = sentence.split(" ")[2]
-           m1=True
+            t1 = sentence.split(" ")[2]
+            m1 = True
         elif match_2 and not m2:
-           t2 = sentence.split(" ")[2]
-           m2=True
+            t2 = sentence.split(" ")[2]
+            m2 = True
         else:
-           continue
+            continue
 
         # compute the time
         if m1 and m2:
-           format_string = "%H:%M:%S"
-           d1 = datetime.strptime(t1, format_string)
-           d2 = datetime.strptime(t2, format_string)
-           delta = d2 - d1
-           t = delta.seconds
-           break
+            format_string = "%H:%M:%S"
+            d1 = datetime.strptime(t1, format_string)
+            d2 = datetime.strptime(t2, format_string)
+            delta = d2 - d1
+            t = delta.seconds
+            break
     return t
 
 
@@ -242,12 +209,14 @@ def get_cuda_graph_time(logs):
         match = re.search(pattern, sentence)
 
         if match:
-            new_sentence=sentence.split("]")
-            match = re.findall(r"-?\d+\.\d+|-?\d+", new_sentence[1])  # The regex pattern matches one or more digits (\d+)
+            new_sentence = sentence.split("]")
+            match = re.findall(
+                r"-?\d+\.\d+|-?\d+", new_sentence[1]
+            )  # The regex pattern matches one or more digits (\d+)
 
             if match:
-               t = match[0]
-               break
+                t = match[0]
+                break
             else:
                 continue
         else:
@@ -256,7 +225,7 @@ def get_cuda_graph_time(logs):
 
 
 # Extract init engine time
-def get_apiserver_init_time (logs):
+def get_apiserver_init_time(logs):
     d = None
     pattern = "init engine"
 
@@ -265,27 +234,34 @@ def get_apiserver_init_time (logs):
         match = re.search(pattern, sentence)
 
         if match:
-           current_datetime = datetime.now()
-           current_year = current_datetime.year
+            current_datetime = datetime.now()
+            current_year = current_datetime.year
 
-           t = str(current_year) + "-" + sentence.split(" ")[1] + " " + sentence.split(" ")[2]
+            t = (
+                str(current_year)
+                + "-"
+                + sentence.split(" ")[1]
+                + " "
+                + sentence.split(" ")[2]
+            )
 
-           # convert to UTC timezone:
-           d = convert_pt_to_utc(t)
-           break
+            # convert to UTC timezone:
+            d = convert_pt_to_utc(t)
+            break
         else:
-           continue 
+            continue
     return d
 
 
 # Extract API Readiness time: an approximation for this computation
-def get_apiserver_init_time_simple (logs):
-    pattern1 = "init engine"
-    pattern2 = "Route: /invocations, Methods: POST"
-    
+def get_apiserver_init_time_simple(logs):
+    # pattern1 = "init engine"
+    pattern1 = "Graph capturing finished"
+    pattern2 = "Route: /metrics, Methods: GET"
+
     t = 0
-    m1=False
-    m2=False
+    m1 = False
+    m2 = False
 
     for line in logs:
         sentence = line.strip("\n")
@@ -293,88 +269,117 @@ def get_apiserver_init_time_simple (logs):
         match_2 = re.search(pattern2, sentence)
 
         if match_1 and not m1:
-           t1 = sentence.split(" ")[2]
-           m1=True
+            t1 = sentence.split(" ")[2]
+            m1 = True
         elif match_2 and not m2:
-           t2 = sentence.split(" ")[2]
-           m2=True
+            t2 = sentence.split(" ")[2]
+            m2 = True
         else:
-           continue
+            continue
 
         # compute the time
         if m1 and m2:
-           format_string = "%H:%M:%S"
-           d1 = datetime.strptime(t1, format_string)
-           d2 = datetime.strptime(t2, format_string)
-           delta = d2 - d1
-           t = delta.seconds 
-           break
-    return t
-
-
-# Extract memory profile latency
-def get_memory_profile_time(logs):
-    t = 0
-    pattern = "Memory profiling takes"
-    
-    for line in logs:
-        sentence = line.strip("\n")
-        match = re.search(pattern, sentence)
-
-        if match:
-            new_sentence=sentence.split("]")
-            match = re.findall(r"-?\d+\.\d+|-?\d+", new_sentence[1])  # The regex pattern matches one or more digits (\d+)
-
-            if match:
-               t = match[0]
-               break
-            else:
-                continue
-        else:
-            continue
-    return t
-
-
-# Extract memory profile latency
-def get_model_weight_time(logs):
-    t = 0
-    pattern = "Time took to download weights for"
-    
-    for line in logs:
-        sentence = line.strip("\n")
-        match = re.search(pattern, sentence)
-
-        if match:
-            new_sentence=(sentence.split("]")[1]).split(":")
-            match = re.findall(r"-?\d+\.\d+|-?\d+", new_sentence[1])  # The regex pattern matches one or more digits (\d+)
-
-            if match:
-               t = match[0]
-               break
-            else:
-                continue
-        else:
-            continue
+            format_string = "%H:%M:%S"
+            d1 = datetime.strptime(t1, format_string)
+            d2 = datetime.strptime(t2, format_string)
+            delta = d2 - d1
+            t = delta.seconds
+            break
     return t
 
 
 def convert_string_to_datetime(date_string, format_string):
     try:
-      datetime_object = datetime.strptime(date_string, format_string)
-      return datetime_object
+        datetime_object = datetime.strptime(date_string, format_string)
+        return datetime_object
 
     except ValueError:
-      return None
+        return None
 
 
-def convert_pt_to_utc(pt_datetime_str, pt_timezone_str='US/Pacific'):
+def convert_pt_to_utc(pt_datetime_str, pt_timezone_str="US/Pacific"):
     pt_timezone = pytz.timezone(pt_timezone_str)
     pt_datetime = datetime.strptime(pt_datetime_str, "%Y-%m-%d %H:%M:%S")
-    
+
     # Make the datetime object timezone-aware
     pt_datetime_aware = pt_timezone.localize(pt_datetime)
-    
+
     # Convert to UTC
     utc_datetime = pt_datetime_aware.astimezone(pytz.utc)
-    
     return utc_datetime
+
+
+# Extract Time to Sleep
+def get_time_to_sleep(logs):
+    t = 0
+    pattern = "seconds to fall asleep"
+
+    for line in logs:
+        sentence = line.strip("\n")
+        match = re.search(pattern, sentence)
+
+        if match:
+            new_sentence = sentence.split("]")
+            match = re.findall(
+                r"-?\d+\.\d+|-?\d+", new_sentence[1]
+            )  # The regex pattern matches one or more digits (\d+)
+
+            if match:
+                t = match[0]
+                break
+            else:
+                continue
+        else:
+            continue
+    return t
+
+
+# Extract sleeping GPU memory freed in GB
+def get_sleep_memory_freed_kept(logs):
+    t1 = 0
+    t2 = 0
+    pattern = "Sleep mode freed"
+
+    for line in logs:
+        sentence = line.strip("\n")
+        match = re.search(pattern, sentence)
+
+        if match:
+            new_sentence = (sentence.split("]")[1]).split(",")
+            match1 = re.findall(r"-?\d+\.\d+|-?\d+", new_sentence[0])
+            match2 = re.findall(r"-?\d+\.\d+|-?\d+", new_sentence[1])
+
+            if match1 and match2:
+                t1 = match1[0]
+                t2 = match2[0]
+                break
+            else:
+                continue
+        else:
+            continue
+    return t1, t2
+
+
+# Extract Time to Wake_up
+def get_time_to_wake(logs):
+    t = 0
+    pattern = "seconds to wake up tags"
+
+    for line in logs:
+        sentence = line.strip("\n")
+        match = re.search(pattern, sentence)
+
+        if match:
+            new_sentence = sentence.split("]")
+            match = re.findall(
+                r"-?\d+\.\d+|-?\d+", new_sentence[1]
+            )  # The regex pattern matches one or more digits (\d+)
+
+            if match:
+                t = match[0]
+                break
+            else:
+                continue
+        else:
+            continue
+    return t
